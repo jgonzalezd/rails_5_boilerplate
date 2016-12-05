0d3fca17250da69d2771f7257f71d471c6225158
Julian Gonzalez <jgonzalezd.sist@gmail.com>
2016-12-05 12:46:06 -0500

    disabled frontend generators

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module BoilerplateRails5
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators.assets = false
+    config.generators.helper = false    
+    config.generators.template_engine = false
   end
 end
