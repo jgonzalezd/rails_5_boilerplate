eb3ace1fa95d7629c337796509cfe8169aaa06bf
Julian Gonzalez <juliangonzalez.code@gmail.com>
2017-02-03 11:13:22 -0500

    added basic breakfast

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ end
 
 group :development do
   gem 'annotate'
+  gem "breakfast"
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console'
   gem 'listen', '~> 3.0.5'
---
+++ app/frontend/js/app.js
@@ -0,0 +1,14 @@
+// Turbolinks - To disable remove the next two lines.
+// https://github.com/turbolinks/turbolinks
+const Turbolinks = require("turbolinks");
+Turbolinks.start();
+
+// Require https://github.com/rails/jquery-ujs
+require("jquery-ujs");
+
+let App = {
+  init() {
+  }
+}
+
+module.exports = App;
